8e5626b2582b1187f495e229a4043d823cffa07d
mokasp <kasperbouldin@gmail.com>
2024-06-28 07:58:24 -0500

    architecture variation from notebook

---
+++ advanced_gan/Architecture-Variation/Smaller/AVSmaller_model.py
@@ -0,0 +1,107 @@
+# specific functions related to neural networks
+import torch.nn as nn
+
+from AVSmaller_config import *
+
+
+class Generator(nn.Module):
+
+  def __init__(self):
+    super(Generator, self).__init__()
+
+    self.network = nn.Sequential(
+        nn.ConvTranspose2d(
+
+            in_channels=LATENT_DIMS,
+            out_channels=NUM_GEN_KERN * 4  ,
+            kernel_size=4,
+            stride=4,
+            padding=0,
+            bias=False
+            ),
+        nn.BatchNorm2d(NUM_GEN_KERN * 4),
+        nn.ReLU(True),
+        nn.ConvTranspose2d(
+            in_channels=NUM_GEN_KERN * 4,
+            out_channels=NUM_GEN_KERN * 2,
+            kernel_size=4,
+            stride=4,
+            padding=0,
+            bias=False
+            ),
+        nn.BatchNorm2d(NUM_GEN_KERN * 2),
+        nn.ReLU(True),
+        nn.ConvTranspose2d(
+            in_channels=NUM_GEN_KERN * 2,
+            out_channels=NUM_GEN_KERN,
+            kernel_size=4,
+            stride=2,
+            padding=1,
+            bias=False
+            ),
+        nn.BatchNorm2d(NUM_GEN_KERN),
+        nn.ReLU(True),
+        nn.ConvTranspose2d(
+            in_channels=NUM_GEN_KERN,
+            out_channels=NUM_CHANNELS,
+            kernel_size=4,
+            stride=2,
+            padding=1,
+            bias=False
+            ),
+        nn.Tanh(),
+    )
+
+  def forward(self, input):
+    return self.network(input)
+
+
+
+class Discriminator(nn.Module):
+
+  def __init__(self):
+    super(Discriminator, self).__init__()
+
+    self.network = nn.Sequential(
+        nn.Conv2d(
+            in_channels=NUM_CHANNELS,
+            out_channels=NUM_DISC_KERN,
+            kernel_size=4,
+            stride=2,
+            padding=1,
+            bias=False
+            ),
+        nn.LeakyReLU(0.2, inplace=True),
+        nn.Conv2d(
+            in_channels=NUM_DISC_KERN,
+            out_channels=NUM_DISC_KERN * 2,
+            kernel_size=4,
+            stride=2,
+            padding=1,
+            bias=False
+            ),
+        nn.BatchNorm2d(NUM_DISC_KERN * 2),
+        nn.LeakyReLU(0.2, inplace=True),
+        nn.Conv2d(
+            in_channels=NUM_DISC_KERN * 2,
+            out_channels=NUM_DISC_KERN * 4,
+            kernel_size=4,
+            stride=4,
+            padding=0,
+            bias=False
+            ),
+        nn.BatchNorm2d(NUM_DISC_KERN * 4),
+        nn.LeakyReLU(0.2, inplace=True),
+        nn.Conv2d(
+            in_channels=NUM_DISC_KERN * 4,
+            out_channels=NUM_CHANNELS,
+            kernel_size=4,
+            stride=4,
+            padding=0,
+            bias=False
+            ),
+        nn.Sigmoid()
+        )
+
+  def forward(self, input):
+    return self.network(input)
--- advanced_gan/Baseline/baseline_train.py
+++ advanced_gan/Baseline/baseline_train.py
@@ -3,6 +3,7 @@ import random
 import wandb
 import torch
 import torchvision.utils as vutils
+import torch.nn.functional as F
 from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,9 +14,8 @@ from baseline_utils import *
 
 def run():
 
-    # initialize projectname and config in wandb
     wandb.init(
-        project='BASELINE-DCGAN',
+        project='CAT-GAN',
         config={
         'num_samples': NUM_SAMPLES,
         'run_name': RUN_NAME,
@@ -33,85 +33,68 @@ def run():
 
     config = wandb.config
 
-    # create logs folder
     if not os.path.exists('/logs'):
       os.mkdir('/logs')
     if not os.path.exists(f'/logs/{config.run_name}'):
       os.mkdir(f'/logs/{config.run_name}')
 
-    # set random seed
     manual_seed = config.seed
     random.seed(manual_seed)
     torch.manual_seed(manual_seed)
 
-
-    # set cuda
     if torch.cuda.is_available():
       torch.cuda.manual_seed(manual_seed)
       torch.cuda.manual_seed_all(manual_seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-    # create data loader
     train_loader = prepare_data(config.batch_size)
 
-    # initialize models
-    generator, discriminator, gen_optimizer, disc_optimizer, calc_loss = model_setup()
+    generator, discriminator, gen_optimizer, disc_optimizer = model_setup()
 
-    # store losses
     gen_losses = []
     disc_losses = []
     real_confs = []
     fake_confs = []
     generated_confs = []
 
-    # enable training
     discriminator.train()
     generator.train()
 
     for epoch in range(1, config.epochs + 1):
 
         for batch_idx, (images, labels) in enumerate(tqdm(train_loader)):
-
-
-            # train setup
-            # grab a batch of images
             actual_images, real_batch_size, real_label, fake_label = train_setup(images, discriminator)
 
-            # train discrim on real images
+            # 1: train discrim
             reset_gradients(discriminator)
 
             out = discriminator(actual_images).view(-1)
-            real_loss = calc_loss(out, real_label)
-            real_loss.backward()
+            real_loss = F.binary_cross_entropy(out, real_label)
             real_conf = real_loss.mean().item()
 
 
-            # 2: generate noise vector to generate image from
+            # 2: full pass on generated images
             noise = torch.randn(config.batch_size, config.latent_dims, 1, 1, device=DEVICE)
 
-            # train discriminator on fake images
             fake_images = generator(noise)
             out = discriminator(fake_images.detach()).view(-1)
-            fake_loss = calc_loss(out, fake_label)
-            fake_loss.backward()
+            fake_loss = F.binary_cross_entropy(out, fake_label)
             fake_conf = fake_loss.mean().item()
-
-            # find discriminator loss and perform step
             discriminator_loss = real_loss + fake_loss
+            discriminator_loss.backward()
             disc_optimizer.step()
 
             # 3: train generator
             reset_gradients(generator)
 
             out = discriminator(fake_images).view(-1)
-            generator_loss = calc_loss(out, real_label)
+            generator_loss = F.binary_cross_entropy(out, real_label)
             generator_loss.backward()
             generated_conf = generator_loss.mean().item()
 
             gen_optimizer.step()
 
-            # track step metrics with wandb
             s_metrics = {
                 'discriminator_loss': discriminator_loss.item(),
                 'generator_loss': generator_loss.item(),
@@ -120,7 +103,6 @@ def run():
                 'generated_conf': generated_conf
             }
 
-            # collect losses and confidences
             gen_losses.append(generator_loss.item())
             disc_losses.append(discriminator_loss.item())
             real_confs.append(real_conf)
@@ -133,7 +115,6 @@ def run():
         fake_conf = sum(fake_confs) / len(fake_confs)
         generated_conf = sum(generated_confs) / len(generated_confs)
 
-        # track epoch metrics with wandb
         e_metrics = {
             'discriminator_loss': discriminator_loss,
             'generator_loss': generator_loss,
@@ -141,18 +122,15 @@ def run():
             'fake_conf': fake_conf,
             'generated_conf': generated_conf,
         }
-
         wandb.log({**s_metrics, **e_metrics})
-
         print(f'Epoch ', epoch, '/', config.epochs, ': Discriminator Loss: ', sum(disc_losses) / len(disc_losses), 'Generator: ', sum(gen_losses) / len(gen_losses))
 
-        # display and save generated images after training
         samples = []
         static_noise = torch.randn(real_batch_size, config.latent_dims, 1, 1, device=DEVICE)
         generator.eval()
         samples = generator(static_noise)
 
-        grid = vutils.make_grid(samples[:24], normalize=True, scale_each=True)
+        grid = vutils.make_grid(samples[:25], normalize=True, nrow=5, scale_each=True)
         grid = grid.permute(1, 2, 0).cpu().numpy()
         grid = (grid * 255).astype(np.uint8)
         wandb.log({'samples': wandb.Image(grid)})
@@ -161,9 +139,11 @@ def run():
             plt.figure(figsize=(10, 10))
             for x in range(25):
                 sample = samples[x]
-                sample = np.reshape(sample.cpu().detach().numpy(), (28, 28))
+                sample = sample.cpu().detach().numpy()
+                sample = sample.transpose(1, 2, 0)
+                sample = (sample + 1) / 2
                 plt.subplot(5, 5, x + 1)
-                plt.imshow(sample, cmap='gray')
+                plt.imshow(sample)
                 plt.axis('off')
             plt.show()
         generator.train()
